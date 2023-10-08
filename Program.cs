0c100e04224147169222e6c7ac5e587c40c5d658
Maciej Kaczmarczyk <maciej.kaczmarczyk01@gmail.com>
2023-10-09 00:20:43 +0200

    zadania 07_10

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PO2023
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var zadania0810 = new Zadania_07_10();
+            zadania0810.zad2_1();
+            zadania0810.zad2_2();
+            zadania0810.zad2_3();
+            zadania0810.zad2_4();
+            zadania0810.zad2_5();
+            zadania0810.zad2_6();
+            zadania0810.zad2_7();
+            zadania0810.zad2_8();
+            zadania0810.zad2_9();
+            zadania0810.zad2_10();
+        }
+    }
+}
+
