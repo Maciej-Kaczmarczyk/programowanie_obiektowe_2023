9679bf15c1498ec8fcfe2342d30593654569e4dc
Maciej Kaczmarczyk <maciej.kaczmarczyk01@gmail.com>
2023-11-16 19:41:53 +0100

    zadania 22.10

--- Program.cs
+++ Program.cs
@@ -5,16 +5,31 @@
         public static void Main()
         {
             var zadania0810 = new Zadania_07_10();
-            zadania0810.zad2_1();
-            zadania0810.zad2_2();
-            zadania0810.zad2_3();
-            zadania0810.zad2_4();
-            zadania0810.zad2_5();
-            zadania0810.zad2_6();
-            zadania0810.zad2_7();
-            zadania0810.zad2_8();
-            zadania0810.zad2_9();
-            zadania0810.zad2_10();
+            var zadania2210 = new Zadania_22_10();
+            //zadania0810.zad2_1();
+            //zadania0810.zad2_2();
+            //zadania0810.zad2_3();
+            //zadania0810.zad2_4();
+            //zadania0810.zad2_5();
+            //zadania0810.zad2_6();
+            //zadania0810.zad2_7();
+            //zadania0810.zad2_8();
+            //zadania0810.zad2_9();
+            //zadania0810.zad2_10();
+            //zadania2210.zad3_1();
+            //zadania2210.zad3_2();
+            //zadania2210.zad3_3();
+            //zadania2210.zad3_4();
+            //zadania2210.zad3_5();
+            //zadania2210.zad3_6();
+            //zadania2210.zad3_8();
+            //zadania2210.zad3_9();
+            //zadania2210.zad3_10();
+            //zadania2210.zad3_11();
+            //zadania2210.zad3_12();
+            //zadania2210.zad3_13();
+            //zadania2210.zad3_14();
+            zadania2210.zad3_15();
         }
     }
 }
